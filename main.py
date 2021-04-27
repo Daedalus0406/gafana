373c077fc86ed37435cf5bd23e1a434bf61ffd58
Daedalus Chang <imagineer0406@gmail.com>
2021-04-28 01:39:00 +0800

    update 1

--- main.py
+++ main.py
@@ -16,17 +16,17 @@ ip = 'http://203.75.178.67:8080/api/datasources/proxy/1/'
 name = 'iotdbfa'
 measure = 'tank'
 ad = 'vpi3860'
-timestamp1 = '1619193600000ms'
-timestamp2 = '1619279999999ms'
+time1 = '1619193600000ms'
+time2 = '1619279999999ms'
 
-def crawler(dbip, dbname, measurement, address, timestamp1):
+def crawler(dbip, dbname, measurement, address, timestamp1, timestamp2):
     url = f"{dbip}query?db={dbname}&q=SELECT * FROM \"{measurement}\" WHERE \"device\"= \'{address}\' " \
-          f"and time >= {timestamp1} and time <= {timestamp2} "
+          f"and time >= {timestamp1}  "
 
     return url
 
 # url_3800 = http://203.75.178.67:8080/api/datasources/proxy/1/query?db=iotdbfa&q=SELECT last("infusion_vacuum") FROM "tank" WHERE ("device" = 'vpi3860') AND time >= 1619280000000ms GROUP BY time(1m)"
-url_3800 = crawler(ip, name, measure, ad, timestamp1)
+url_3800 = crawler(ip, name, measure, ad, time1, time2)
 
 r_3800 = requests.get(url=url_3800, params='iotdbfa' ,auth=(user, password))
 js = r_3800.json()
@@ -46,7 +46,7 @@ df['time'] = pd.to_datetime(df['time'], format="%Y-%m-%d %H:%M")
 # df['time'] = df['time'].apply(lambda _: datetime.strptime(_,"%Y-%m-%d %H:%M"))
 
 # print(df.time)
-'''
+
 fig, ax1 = plt.subplots()
 plt.title('Infusion Vacuum & Pressure')
 plt.xlabel('time')
@@ -58,14 +58,14 @@ ax1.plot(df.time, df.infusion_vacuum, color='tab:blue')
 ax1.tick_params(axis='y', labelcolor='tab:blue')
 
 ax2.set_ylabel('Pressure', color='black')
-ax2.plot(df.time, df.infusion_pressure, color='tab:blue')
+ax2.plot(df.time, df.infusion_pressure, color='black')
 ax2.tick_params(axis='y', labelcolor='black')
 
 fig.tight_layout()
 plt.show()
-'''
-profile = pandas_profiling.ProfileReport(df)
-profile.to_file("output.html")
+
+# profile = pandas_profiling.ProfileReport(df)
+# profile.to_file("output.html")
 
 df.to_csv("test_38.csv")
 
